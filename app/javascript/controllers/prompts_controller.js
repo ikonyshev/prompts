5ad438acda7be486e9cba58f4ec5c90c140d9f0b
Ivan Konyshev <ikonyshev@intellpro.com>
2023-11-24 00:00:38 +0200

    Some js optimization

--- app/javascript/controllers/prompts_controller.js
+++ app/javascript/controllers/prompts_controller.js
@@ -5,40 +5,31 @@ export default class extends Controller {
   static targets = ["suggestions", "vectorSuggestions"]
 
   update(e) {
-    if (e.target.value.length >= 3) {
-      Rails.ajax({
-        type: 'GET',
-        url: '/prompts/search',
-        data: new URLSearchParams({
-          query: e.target.value
-        }).toString(),
-        success: (data) => {
-          this.suggestionsTarget.innerHTML = data.length > 0 ? data.map(suggestion =>
-            `<li>${suggestion.text} <span class="font-bold">${suggestion.score}</span></li>`
-          ).join("") : 'No suggestions found.'
-        },
-        error: function(err) {
-          console.log(err);
-        }
-      });
+    this.#searchSuggestions(e.target.value, this.suggestionsTarget)
+    this.#searchSuggestions(e.target.value, this.vectorSuggestionsTarget, 'vector')
+  }
 
+  #searchSuggestions(query, target, vector = false) {
+    if (query.length >= 3) {
       Rails.ajax({
         type: 'GET',
         url: '/prompts/search',
         data: new URLSearchParams({
-          query: e.target.value,
-          type: 'vector'
+          query: query,
+          type: vector ? 'vector' : ''
         }).toString(),
         success: (data) => {
-          console.log(this)
-          this.vectorSuggestionsTarget.innerHTML = data.length > 0 ? data.map(suggestion =>
+          target.innerHTML = data.length > 0 ? data.map(suggestion =>
             `<li>${suggestion.text} <span class="font-bold">${suggestion.score}</span></li>`
-          ).join("") : 'No suggestions found.'
+          ).join("") : "No suggestions found."
         },
         error: function(err) {
           console.log(err);
         }
       });
+    } else {
+      target.innerHTML = "No suggestions found."
     }
   }
+
 }
